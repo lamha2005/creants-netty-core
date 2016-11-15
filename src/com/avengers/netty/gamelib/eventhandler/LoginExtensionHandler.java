eec177880ea6b984874f75ae238e5b6931428169
hamala <hamanhlam2005@gmail.com>
2016-11-15 23:29:37 +0700

    Test commit

--- src/com/avengers/netty/gamelib/eventhandler/LoginExtensionHandler.java
+++ src/com/avengers/netty/gamelib/eventhandler/LoginExtensionHandler.java
@@ -5,6 +5,7 @@ package com.avengers.netty.gamelib.eventhandler;
 
 import com.avengers.netty.core.event.ICoreEvent;
 import com.avengers.netty.core.extensions.BaseServerEventHandler;
+import com.avengers.netty.core.extensions.CoreExtension;
 
 /**
  * @author LamHa
@@ -14,7 +15,7 @@ public class LoginExtensionHandler extends BaseServerEventHandler {
 
 	@Override
 	public void handleServerEvent(ICoreEvent event) {
-		// CoreExtension parentExtension = getParentExtension();
+		CoreExtension parentExtension = getParentExtension();
 		// User user = (User) event.getParameter(CoreEventParam.USER);
 
 	}
